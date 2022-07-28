86208ba94be06565922806a47cd0145dfa6734b1
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-07-28 04:58:26 +0200

    feat: setup heroku db connection string

---
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/config/HerokuPostgresConfig.java
@@ -0,0 +1,37 @@
+package com.vegari1.devops.agentapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "stage",
+        havingValue = "test"
+)
+public class HerokuPostgresConfig {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
