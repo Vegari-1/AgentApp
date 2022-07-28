d409e5f215269a3e11d80dd7419adb71bfef0b2b
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-07-28 05:47:17 +0200

    fix: change cors config default value

--- agent-app/src/main/java/com/vegari1/devops/agentapp/config/CorsConfig.java
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.vegari1.devops.agentapp.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,10 +10,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConditionalOnProperty(
-        value = "stage"
-//        havingValue = "prod"
-)
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 
