93e7971806d43444b8a25eb7e5a545a5e815d3b7
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-07-28 02:59:39 +0200

    feat: create parametrised docker-compose

--- agent-app/src/main/java/com/vegari1/devops/agentapp/config/CorsConfig.java
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.vegari1.devops.agentapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,18 +11,27 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConditionalOnProperty(
+        value = "stage"
+//        havingValue = "prod"
+)
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Value("${cors.origin}")
+    private String corsOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")
-                .allowedOrigins("http://localhost:4200");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOrigins(corsOrigin);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
+
 }
+
