6dd47e87b70cedb25345245a57339b1d6960f46d
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-08-04 22:01:28 +0200

    test: integration testing with testcontainers

--- agent-app/src/test/java/com/vegari1/devops/agentapp/AgentAppApplicationTests.java
+++ agent-app/src/test/java/com/vegari1/devops/agentapp/AgentAppApplicationTests.java
@@ -1,13 +1,32 @@
 package com.vegari1.devops.agentapp;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
-class AgentAppApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
-	//@Test
-	void contextLoads() {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class AgentAppApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testShouldReturnGreeting() throws Exception {
+		assertThat(this.restTemplate.getForObject(
+				"http://localhost:" + port + "/api/test", String.class))
+				.contains("Hello from the back");
 	}
 
 }
