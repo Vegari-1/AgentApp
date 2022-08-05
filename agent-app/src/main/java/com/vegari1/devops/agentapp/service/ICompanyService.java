c9456d4f3c395f6aa43ffe9ab15da412a4d4fc1d
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-08-05 03:18:28 +0200

    feat: accept and decline company request

--- agent-app/src/main/java/com/vegari1/devops/agentapp/service/ICompanyService.java
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/service/ICompanyService.java
@@ -1,10 +1,15 @@
 package com.vegari1.devops.agentapp.service;
 
 import com.vegari1.devops.agentapp.exception.EntityExistsException;
+import com.vegari1.devops.agentapp.model.Company;
 import com.vegari1.devops.agentapp.model.CompanyRegistrationRequest;
 
 public interface ICompanyService {
 
     CompanyRegistrationRequest createCompanyRegReq(CompanyRegistrationRequest companyRegReq)
             throws EntityExistsException;
+
+    Company acceptCompanyRequest(Long companyRequestId);
+
+    void declineCompanyRequest(Long companyRequestId);
 }
--- agent-app/src/main/java/com/vegari1/devops/agentapp/service/impl/UserService.java
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/service/impl/UserService.java
@@ -12,12 +12,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.swing.text.html.parser.Entity;
 
 @AllArgsConstructor
 @Service
@@ -42,10 +39,10 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public String login(User user) throws AuthenticationException {
+    public String login(User user) throws BadCredentialsException {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return tokenUtils.generateToken(user.getUsername(), user.getAuthorities());
+        return tokenUtils.generateToken(user.getUsername(), authentication.getAuthorities());
     }
 }
