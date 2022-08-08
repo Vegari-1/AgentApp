c9d6386f36a407d2f9b9c146743898dd5b803840
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-08-08 03:40:18 +0200

    feat: get job offer by id, get job offer by company id

--- agent-app/src/main/java/com/vegari1/devops/agentapp/controller/JobOfferController.java
+++ agent-app/src/main/java/com/vegari1/devops/agentapp/controller/JobOfferController.java
@@ -15,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -31,6 +32,22 @@ public class JobOfferController {
             @Valid @RequestBody JobOfferRequest jobOfferRequest) {
         JobOffer jobOffer = jobOfferService.createJobOffer(jobOfferMapper.toEntity(jobOfferRequest), companyId);
         return new ResponseEntity<>(jobOfferMapper.toResponse(jobOffer), HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/{jobOfferId}")
+    public ResponseEntity<JobOfferResponse> getJobOfferById(
+            @PathVariable Long jobOfferId) {
+        JobOffer jobOffer = jobOfferService.getJobOfferById(jobOfferId);
+        return new ResponseEntity<>(jobOfferMapper.toResponse(jobOffer), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/company/{companyId}")
+    public ResponseEntity<List<JobOfferResponse>> getAllCompanyJobOffers(
+            @PathVariable Long companyId) {
+        List<JobOffer> jobOffers = jobOfferService.getJobOfferByCompanyId(companyId);
+        return new ResponseEntity<>(jobOfferMapper.toResponseList(jobOffers), HttpStatus.OK);
     }
 
 }
